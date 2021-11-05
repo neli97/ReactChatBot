9106f1226ebf7d5031bd32b431dc5ecacbfd8795
neli97 <neli.zarkova@abv.bg>
2021-11-05 09:38:26 +0200

    Heroku, Nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({'hello': 'there'});
+    res.send({'hello': 'Neli'});
 })
 
 const PORT = process.env.PORT || 5000;
